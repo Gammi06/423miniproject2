3fda6aa1a851b34ce054885e8690c05a98bbdc12
mysjw1028 <108706248+mysjw1028@users.noreply.github.com>
2022-11-01 17:52:18 +0900

    Companys , myskills, subribes Dto작업 완료 (#6)
    * Dto이름 수정
    
    * WantedSkillsRespDto 파일 위치 수정 완료
    
    * MySkills 이름 확인 했습니다.

---
+++ src/main/java/site/metacoding/miniproject2/dto/MySkillsRespDto.java
@@ -0,0 +1,20 @@
+package site.metacoding.miniproject2.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/*>>>>>>지원 작업함<<<<<<< */
+/*기존 작업자 승현 */
+public class MySkillsRespDto {
+
+    @Setter
+    @Getter
+    public static class WantedsSkillsRespDto {
+        private Integer id;
+        private Integer wantedId;
+        private Integer skillsCodeId;
+        private String skillsCodeName;
+    }
+
+}
+/* >>>>>>>>지원 작업 종료<<<<<<<<<<<< */
---
+++ src/main/java/site/metacoding/miniproject2/dto/SubribesRespDto.java
@@ -0,0 +1,32 @@
+package site.metacoding.miniproject2.dto;
+
+import java.sql.Timestamp;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+/*>>>>>>지원 작업함<<<<<<< */
+/*기존 작업자 지원 */
+public class SubribesRespDto {
+
+    @ToString
+    @Getter
+    @Setter
+    public static class SubscribesListRespDto {
+        private Integer Id;
+        private Integer companysId;
+        private Integer wantedsId;
+        private String companyName;
+        private Timestamp created;// 공고시작시간
+        private Timestamp enddate;// 공고끝나는시간
+        private String state;
+
+    }
+
+}
+/*
+ * >>>>>>>>지원 작업 종료<<<<<<<<<<<
+ * datePart,day :겹치는 내용
+ * private Timestamp currnetTimestamp; :현재시간 필요하지 않으듯 해서 삭제
+ */
