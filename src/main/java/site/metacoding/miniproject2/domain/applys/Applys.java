4ad9a7b1a93c5288af3938c389106b622e2fd4e0
SINGIN <104334760+Gammi06@users.noreply.github.com>
2022-11-01 16:45:23 +0900

    Apply, Like Dto 작업 및 Service 작업완료함 (#1)
    * apply와 like의 Req, Resp버전으로 두 개 제작함
    
    * apply와 like의 dto 수정함
    
    * apply와 like의 dto 수정 및 추가함, service 생성함
    
    * service 구현함
    
    * dto이름 수정함

--- src/main/java/site/metacoding/miniproject2/domain/applys/Applys.java
+++ src/main/java/site/metacoding/miniproject2/domain/applys/Applys.java
@@ -10,14 +10,16 @@ import lombok.NoArgsConstructor;
 @Getter
 public class Applys {
     private Integer id;
-	private Integer wantedId;
-	private Integer status;
-	private Timestamp created;
+    private Integer wantedId;
+    private Integer resumeId;
+    private Integer status;
+    private Timestamp created;
 
     @Builder
-    public Applys(Integer id, Integer wantedId, Integer status, Timestamp created) {
+    public Applys(Integer id, Integer wantedId, Integer resumeId, Integer status, Timestamp created) {
         this.id = id;
         this.wantedId = wantedId;
+        this.resumeId = resumeId;
         this.status = status;
         this.created = created;
     }
--- src/main/java/site/metacoding/miniproject2/domain/likes/Likes.java
+++ src/main/java/site/metacoding/miniproject2/domain/likes/Likes.java
@@ -2,6 +2,7 @@ package site.metacoding.miniproject2.domain.likes;
 
 import java.sql.Timestamp;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -12,11 +13,12 @@ public class Likes {
     private Integer userId;
     private Integer wantedId;
     private Timestamp created;
-    
+
+    @Builder
     public Likes(Integer id, Integer userId, Integer wantedId, Timestamp created) {
         this.id = id;
         this.userId = userId;
         this.wantedId = wantedId;
         this.created = created;
-    }    
+    }
 }
