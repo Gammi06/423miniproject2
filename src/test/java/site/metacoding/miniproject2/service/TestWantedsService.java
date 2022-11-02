fd81542c9e65469bad4e7c2dee714bd64d2f6e0b
SINGIN <104334760+Gammi06@users.noreply.github.com>
2022-11-02 17:26:03 +0900

    Wanted 테스트용 가상 컨트롤러, 서비스 제작완료함

---
+++ src/test/java/site/metacoding/miniproject2/service/TestWantedsService.java
@@ -0,0 +1,67 @@
+package site.metacoding.miniproject2.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+import site.metacoding.miniproject2.domain.applys.ApplysDao;
+import site.metacoding.miniproject2.domain.likes.LikesDao;
+import site.metacoding.miniproject2.domain.wanteds.WantedsDao;
+import site.metacoding.miniproject2.dto.WantedsRespDto.SearchDto;
+import site.metacoding.miniproject2.dto.WantedsRespDto.WantedDetailRespDto;
+import site.metacoding.miniproject2.dto.WantedsRespDto.WantedListRespDto;
+
+@RequiredArgsConstructor
+@Service
+public class TestWantedsService {
+    private final WantedsDao wantedsDao;
+    private final ApplysDao applysDao;
+    private final LikesDao likesDao;
+
+    public void insert() {
+        wantedsDao.insert();
+    }
+
+    public List<WantedListRespDto> findAll(SearchDto searchDto) {
+        if (searchDto == null) {
+            List<WantedListRespDto> wantedList = wantedsDao.findAll();
+            return wantedList;
+        }
+        List<WantedListRespDto> wantedList = wantedsDao.findAllOrdered(searchDto);
+        return wantedList;
+    }
+
+    public WantedDetailRespDto findById(Integer id) {
+        WantedDetailRespDto wantedPS = wantedsDao.findById(id);
+        return wantedPS;
+    }
+
+    public List<WantedListRespDto> findAllByCompanyId(Integer companyId) {
+        // company의 findById 추가하기
+        List<WantedListRespDto> wantedList = wantedsDao.findAllByCompanyId(companyId);
+        return wantedList;
+    }
+
+    public List<WantedListRespDto> findAllByPositionCodeName(String positionCodeId) {
+        // position의 findById 추가하기
+        // id를 string으로 바꾸는 코드 추가하기
+        String positionCodeName = null;
+        List<WantedListRespDto> wantedList = wantedsDao.findAllByPositionCodeName(positionCodeName);
+        return wantedList;
+    }
+
+    public List<WantedListRespDto> findAllByLike(Integer userId) {
+        // 유저있는지 확인
+        List<WantedListRespDto> wantedList = wantedsDao.findAllByLike(userId);
+        return wantedList;
+    }
+
+    public void wantedInsertLike(Integer userId) {
+        likesDao.insert();
+    }
+
+    public void wantedInsertApply(Integer userId) {
+        applysDao.insert();
+    }
+}
---
+++ src/test/java/site/metacoding/miniproject2/web/TestWantedApiController.java
@@ -0,0 +1,59 @@
+package site.metacoding.miniproject2.web;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+import site.metacoding.miniproject2.dto.WantedsRespDto.SearchDto;
+import site.metacoding.miniproject2.dto.WantedsRespDto.WantedDetailRespDto;
+import site.metacoding.miniproject2.dto.WantedsRespDto.WantedListRespDto;
+import site.metacoding.miniproject2.service.TestWantedsService;
+
+@RequiredArgsConstructor
+@RestController
+public class TestWantedApiController {
+    private final TestWantedsService wantedsService;
+    // private final LikesService likesService;
+    // private final ApplyService applyService;
+
+    @GetMapping("/api/wanted")
+    public List<WantedListRespDto> findAll(SearchDto searchDto) {
+        return wantedsService.findAll(searchDto);
+    }
+
+    @GetMapping("/api/wanted/{wantedId}")
+    public WantedDetailRespDto findById(@PathVariable Integer wantedId) {
+        return wantedsService.findById(wantedId);
+    }
+
+    @GetMapping("/s/api/wanted/{userId}/like")
+    public List<WantedListRespDto> findAllByLike(@PathVariable Integer userId) {
+        return wantedsService.findAllByLike(userId);
+    }
+
+    @GetMapping("/api/wanted/position/{positionCodeName}")
+    public List<WantedListRespDto> findAllByPositionCodeName(@PathVariable String positionCodeName) {
+        return wantedsService.findAllByPositionCodeName(positionCodeName);
+    }
+
+    @GetMapping("/api/wanted/position/{companyId}")
+    public List<WantedListRespDto> findAllByCompanyId(@PathVariable Integer companyId) {
+        return wantedsService.findAllByCompanyId(companyId);
+    }
+
+    @PostMapping("/s/api/wanted/{userId}/like")
+    public void wantedInsertLike(@PathVariable Integer userId) {
+        // userId에 유저 아이디값 넣어주기
+        wantedsService.wantedInsertLike(userId);
+    }
+
+    @PostMapping("/s/api/wanted/{userId}/apply")
+    public void wantedInsertApply(@PathVariable Integer userId) {
+        // userId에 유저 아이디값 넣어주기
+        wantedsService.wantedInsertApply(userId);
+    }
+}
