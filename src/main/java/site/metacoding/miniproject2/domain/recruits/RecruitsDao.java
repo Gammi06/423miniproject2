f7199c9dc6184e4339a9f1cc1a6f5ee8b9064b49
suhyeon <112357365+suucode@users.noreply.github.com>
2022-11-02 14:48:48 +0900

    WantedsService, RecruitsService 수정완료 (#10)
    * RecruitsReqDto, RecruitsRespDto 완료
    
    * WantedsRespDto 작업 완료
    
    * Wanteds, Recruits Resp/Req Dto 수정 완료
    
    * Recruits Dao/Service 수정 완료

--- src/main/java/site/metacoding/miniproject2/domain/recruits/RecruitsDao.java
+++ src/main/java/site/metacoding/miniproject2/domain/recruits/RecruitsDao.java
@@ -1,5 +1,13 @@
 package site.metacoding.miniproject2.domain.recruits;
 
 public interface RecruitsDao {
-    // 이름 규칙에 맞는 메서드명으로 생성해주세요.
+    public void insert();
+
+    public void findAll();
+
+    public void findById();
+
+    public void updateById();
+
+    public void deleteById();
 }
