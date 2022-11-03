6dd2686330d41edf444c914c755aae4dc7a5abce
suhyeon <limmm.mary@gmail.com>
2022-11-03 20:38:56 +0900

    companys,wanteds,recruits Dao/Service 완료 추후 SessionUser 추가

--- src/main/java/site/metacoding/miniproject2/domain/recruits/RecruitsDao.java
+++ src/main/java/site/metacoding/miniproject2/domain/recruits/RecruitsDao.java
@@ -2,11 +2,17 @@ package site.metacoding.miniproject2.domain.recruits;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Param;
+
 import site.metacoding.miniproject2.dto.RecruitsRespDto.RecruitsApplyManageRespDto;
+import site.metacoding.miniproject2.dto.RecruitsRespDto.RecruitsRecommendByPositionRespDto;
 import site.metacoding.miniproject2.dto.RecruitsRespDto.RecruitsRecommendRespDto;
 
 public interface RecruitsDao {
     public List<RecruitsApplyManageRespDto> findApplyManage(Integer companysId);
 
     public List<RecruitsRecommendRespDto> findRecommend(Integer companysId);
+
+    public List<RecruitsRecommendByPositionRespDto> findRecommendByPosition(@Param("companysId") Integer companysId,
+            @Param("positionsCodeId") Integer positionsCodeId);
 }
