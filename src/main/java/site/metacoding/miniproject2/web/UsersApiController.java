1ff45c53f2fe3888c05581ea4225097ab2fa40c5
SungYu05 <tjddb0531@naver.com>
2022-11-03 20:24:44 +0900

    import 수정

--- src/main/java/site/metacoding/miniproject2/web/UsersApiController.java
+++ src/main/java/site/metacoding/miniproject2/web/UsersApiController.java
@@ -1,7 +1,5 @@
 package site.metacoding.miniproject2.web;
 
-import javax.servlet.http.HttpSession;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
