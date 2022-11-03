e62d85cc2d24841ad8fc7d627256e50776a0ac4d
basakk123 <basakk@naver.com>
2022-11-03 16:48:26 +0900

    마이페이지 dao, dto, 매퍼, 서비스, 컨트롤러 작성 완료

--- src/main/java/site/metacoding/miniproject2/web/UsersApiController.java
+++ src/main/java/site/metacoding/miniproject2/web/UsersApiController.java
@@ -2,6 +2,8 @@ package site.metacoding.miniproject2.web;
 
 import javax.servlet.http.HttpSession;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,5 +26,12 @@ public class UsersApiController {
         session.setAttribute("sessionUsers", sessionUsers);
         return new CMRespDto<>(1, "ok", sessionUsers);
     }
+
+    // 서현 작업
+    @GetMapping("/mypage/{id}")
+    public CMRespDto<?> findAllInfo(@PathVariable Integer id) {
+        return new CMRespDto<>(1, "성공", usersService.findAllInfo(id));
+    }
+    // 서현 작업 종료
 
 }
