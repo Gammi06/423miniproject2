e960fafa7474044be1eeb11a7ac0d66fec077426
s2staree <s2staree@gmail.com>
2022-11-04 19:03:26 +0900

    저장

--- src/main/java/site/metacoding/miniproject2/web/ResumesApiController.java
+++ src/main/java/site/metacoding/miniproject2/web/ResumesApiController.java
@@ -15,6 +15,8 @@ import lombok.RequiredArgsConstructor;
 import site.metacoding.miniproject2.domain.resumes.Resumes;
 import site.metacoding.miniproject2.domain.resumes.ResumesDao;
 import site.metacoding.miniproject2.domain.users.UsersDao;
+import site.metacoding.miniproject2.dto.CMRespDto;
+import site.metacoding.miniproject2.dto.ResumesReqDto.ResumeUpdateReqDto;
 import site.metacoding.miniproject2.dto.ResumesRespDto.ResumeListRespDto;
 import site.metacoding.miniproject2.service.ResumesService;
 
@@ -33,20 +35,20 @@ public class ResumesApiController {
     }
 
     // [Create] Insert
-    @PostMapping("/api/resume/write")
+    @PostMapping("/s/api/resume/write")
     public int insert(@RequestBody Resumes resumes) {
         int resumeWrite = resumesDao.insert(resumes);
         return resumeWrite;
     }
 
     // [Read] Selcet (findAll)
-    @GetMapping("/resume/list")
+    @GetMapping("/s/resume/list")
     public String 이력서관리페이지() {
         return "/resume/resume_list";
     }
 
     // [Read] Selcet (findById)
-    @GetMapping("/resume/{id}/list")
+    @GetMapping("/s/resume/{id}/list")
     public String 이력서관리(@PathVariable Integer id, Model model) {
         List<ResumeListRespDto> resumeListRespDtoPS = resumesService.findById(id);
         model.addAttribute("resumeList", resumeListRespDtoPS);
@@ -54,25 +56,17 @@ public class ResumesApiController {
     }
 
     // [Update] Update
-    @PutMapping("/api/resume/{id}/update")
-    public int updateById(@PathVariable Integer id, @RequestBody Resumes resumes) {
-        Resumes resumesPS = resumesDao.findById(resumes.getId());
-        if (resumesPS == null) {
-            throw new RuntimeException("해당 아이디 없음");
-        }
-        int resumeUpdate = resumesDao.updateById(id, resumes);
-        return resumeUpdate;
+    @PutMapping("/s/api/resume/{id}/update")
+    public CMRespDto<?> updateById(@PathVariable Integer id, @RequestBody ResumeUpdateReqDto resumeUpdateReqDto) {
+        resumesService.updateById(resumeUpdateReqDto);
+        return new CMRespDto<>(1, "성공", null);
     }
 
     // [Delete] Delete
-    @DeleteMapping("/api/resume/{id}/delete")
-    public int deleteById(@PathVariable Integer id) {
-        Resumes resumesPS = resumesDao.findById(id);
-        if (resumesPS == null) {
-            throw new RuntimeException("해당 아이디 없음");
-        }
-        int resumeDelete = resumesDao.deleteById(id);
-        return resumeDelete;
+    @DeleteMapping("/s/api/resume/{id}/delete")
+    public CMRespDto<?> deleteById(@PathVariable Integer id) {
+        resumesService.deleteById(id);
+        return new CMRespDto<>(1, "성공", null);
     }
 }
 /* >>>> 연지 작업종료 <<<< */
