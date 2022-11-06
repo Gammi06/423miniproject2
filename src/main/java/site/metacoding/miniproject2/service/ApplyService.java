b4f01cc609d2d169acd51234fa8f9e2a70c249f4
SINGIN <104334760+Gammi06@users.noreply.github.com>
2022-11-06 17:09:41 +0900

    익셉션 핸들러 제작함

--- src/main/java/site/metacoding/miniproject2/service/ApplyService.java
+++ src/main/java/site/metacoding/miniproject2/service/ApplyService.java
@@ -7,11 +7,12 @@ import org.springframework.stereotype.Service;
 import lombok.RequiredArgsConstructor;
 import site.metacoding.miniproject2.domain.applys.ApplysDao;
 import site.metacoding.miniproject2.domain.resumes.ResumesDao;
-import site.metacoding.miniproject2.dto.SessionUsers;
 import site.metacoding.miniproject2.dto.ApplyReqDto.ApplyFindReqDto;
 import site.metacoding.miniproject2.dto.ApplyReqDto.ApplyUserReqDto;
 import site.metacoding.miniproject2.dto.ApplyRespDto.ApplyFindByIdRespDto;
 import site.metacoding.miniproject2.dto.ApplyRespDto.ApplyInsertRespDto;
+import site.metacoding.miniproject2.dto.SessionUsers;
+import site.metacoding.miniproject2.handler.MyApiException;
 
 @RequiredArgsConstructor
 @Service
@@ -25,19 +26,16 @@ public class ApplyService {
 
     public ApplyInsertRespDto insert(ApplyUserReqDto applyUserReqDto) {
         SessionUsers principal = (SessionUsers) session.getAttribute("principal");
-        /*
-         * if (principal.getUserId().isEmpty()) {
-         * throw new RuntimeException("로그인 해주세요.");
-         * }
-         * // 이력서를 작성한 유저와 현재 로그인한 유저의 아이디가 맞는지 비교하기
-         * // if (resumesDao.findById(applyUserReqDto.getResumeId()).getUserId() !=
-         * // principal.getUserId()) {
-         * // throw new RuntimeException("신청하실 이력서가 없습니다.");
-         * // }
-         * if (applyUserReqDto.getResumeId() == 0) {
-         * throw new RuntimeException("신청하실 이력서가 없습니다.");
-         * }
-         */
+
+        if (principal.getUserId().isEmpty()) {
+            throw new MyApiException("로그인 해주세요.");
+        }
+        // 이력서를 작성한 유저와 현재 로그인한 유저의 아이디가 맞는지 비교하기
+        // if (resumesDao.findById(applyUserReqDto.getResumeId()).getUserId() !=
+        // principal.getUserId()) {
+        // throw new RuntimeException("신청하실 이력서가 없습니다.");
+        // }
+
         applysDao.insert(applyUserReqDto);
         return ApplyInsertRespDto.builder()
                 .resumeId(applyUserReqDto.getResumeId())
@@ -48,7 +46,7 @@ public class ApplyService {
     public ApplyFindByIdRespDto findByApplyId(Integer id) {
         SessionUsers principal = (SessionUsers) session.getAttribute("principal");
         if (principal.getUserId().isEmpty()) {
-            throw new RuntimeException("로그인 해주세요.");
+            throw new MyApiException("로그인 해주세요.");
         }
         ApplyFindReqDto applyFindReqDto = ApplyFindReqDto.builder().userId(null).wantedId(id).build();
         return applysDao.findApply(applyFindReqDto);
