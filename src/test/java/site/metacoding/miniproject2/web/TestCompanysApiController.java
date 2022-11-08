48c07d177f6756d76f42b9bb5454e402a6371696
mysjw1028 <108706248+mysjw1028@users.noreply.github.com>
2022-11-08 09:55:57 +0900

    Companys  and subribes junit 테스트중 (#63)
    * Dto이름 수정
    
    * WantedSkillsRespDto 파일 위치 수정 완료
    
    * MySkills 이름 확인 했습니다.
    
    * Service기초세팅 완료
    
    * CompanysService 샘플코드완료(수정예정)
    
    * subcribes 수정할것
    
    * myskills xmㅣinsert추가
    
    * xml주소 수정/ 서비스 기초 틀 완료
    
    * dao부분 수정완료
    
    * 서비스/다오 엔티티부분 디티오 변경 완료
    
    * service 수정할것
    
    * service 추가수정
    
    * service 수정성공
    
    * xml 수정 완료
    
    * pull전 xml저장
    
    * Subcribes Companys로 위치변경
    
    * pull 후 수정
    
    * Subcribes 수정 완료
    
    * subribes수정 완료
    
    * 메소드명 수정완료
    
    * mapper 오류 확인 후 수정
    
    * updatecompany로 수정 완료
    
    * 컨트롤러 테스트 돌리기
    
    * 서버확인
    
    * 서버 돌아가는거 확인 완료
    
    * likeController중
    
    * Subcribes 작동확인 완료
    
    * 회사 업데이트 포스트맨 테스트 완료
    
    * 회사가입 확인완료
    
    * 회사정보 수정 하는 코드 수정
    
    * 사업자 중복체크 쿼리 스트링 체크 확인
    
    * 컨트롤러 테스트 완료
    
    * 컨트롤러 테스트 완료
    
    * 컨트롤러 확인
    
    * subcribesLis 쿼리 수정 완료
    
    * companys delete 수정 완료!
    
    * likes 작업중
    
    * likes완료 dto.dao.쿼리추가
    
    * subscribes 주소변경 완료
    
    * 컨트롤러 / 좋아요페이지 주소 수정 완료
    
    * subscribes /likes의 주소 mypage 위치 변경
    
    * photo 수정 완료 (company insert쿼리)
    
    * 회사정보 삭제시 구인공고 같이 삭제
    
    * test
    
    * principal 변경 완료
    
    * 인터셉터 수정예정
    
    * 회사정보수정 junit 테스트 완료
    
    * 구독하기 junit 수정중
    
    * principal 수정 완료
    
    Co-authored-by: SINGIN <104334760+Gammi06@users.noreply.github.com>

--- src/test/java/site/metacoding/miniproject2/web/TestCompanysApiController.java
+++ src/test/java/site/metacoding/miniproject2/web/TestCompanysApiController.java
@@ -1,7 +1,5 @@
 package site.metacoding.miniproject2.web;
 
-import java.net.http.HttpResponse;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,6 +23,7 @@ import lombok.extern.slf4j.Slf4j;
 import site.metacoding.miniproject2.domain.companys.CompanysDao;
 import site.metacoding.miniproject2.domain.users.UsersDao;
 import site.metacoding.miniproject2.dto.CompanysReqDto.CompanysInsertReqDto;
+import site.metacoding.miniproject2.dto.CompanysReqDto.CompanysUpdateReqDto;
 import site.metacoding.miniproject2.dto.SessionUsers;
 
 @Slf4j
@@ -58,9 +57,9 @@ public class TestCompanysApiController {
     public void sessionInit() {
         session = new MockHttpSession();
         session.setAttribute("principal", SessionUsers.builder()
-                .id(1)
-                .userId("garam1234")
-                .role("일반")
+                .id(2)
+                .userId("Lzm1992")
+                .role("회사")
                 .build());
     }
 
@@ -105,15 +104,14 @@ public class TestCompanysApiController {
         // then
     }
 
-    /* 지원 작업 시작! */
     @Sql(scripts = "classpath:createTest.sql", executionPhase = ExecutionPhase.BEFORE_TEST_METHOD)
     @Test
     public void insert_test() throws Exception {
 
         // given
-        Integer id = 1;
+        Integer id = 6;
         CompanysInsertReqDto companysInsertReqDto = new CompanysInsertReqDto();
-        companysInsertReqDto.setId(6);
+        companysInsertReqDto.setId(id);
         companysInsertReqDto.setCompanyName("고구마 농장");
         companysInsertReqDto.setAddress("부산 노포동");
         companysInsertReqDto.setEmail("ddd@nate.com");
@@ -121,46 +119,60 @@ public class TestCompanysApiController {
         companysInsertReqDto.setRegionCodeName("부산");
         companysInsertReqDto.setIntro("고구마 맛탕 맛집");
         companysInsertReqDto.setYears(2021);
+        companysInsertReqDto.setMemberCount(11);
         companysInsertReqDto.setUsersId(1);
-        String body = om.writeValueAsString(CompanysInsertReqDto);
+        String body = om.writeValueAsString(companysInsertReqDto);
 
+        System.out.println(body);
         // when
         ResultActions resultActions = mvc.perform(
-                MockMvcRequestBuilders.post("/s/api/companys/" + id + "/add")
+                MockMvcRequestBuilders.post("/s/api/companys/add")
                         .content(body)
                         .contentType(APPLICATION_JSON)
-                        .accept(APPLICATION_JSON));
+                        .accept(APPLICATION_JSON)
+                        .session(session));
 
         // then
         MvcResult mvcResult = resultActions.andReturn();
-        System.out.println("디버그 : " + mvcResult.getResponse().getContentAsString());
-        System.out.println("debugggg:" + mvcResult.getResponse().getStatus());
+        System.out.println("debugggg:" + mvcResult.getResponse().getContentAsString());
 
+        resultActions.andExpect(MockMvcResultMatchers.status().isOk());
         resultActions.andExpect(MockMvcResultMatchers.jsonPath("$.code").value(1));
-        // 유저 아이디
+
     }
-    // 회사서비스 가입 테스트 코드
+    // 회사서비스 가입 테스트 코드 성공
 
     @Sql(scripts = "classpath:createTest.sql", executionPhase = ExecutionPhase.BEFORE_TEST_METHOD)
     @Test
     public void updateCompanyId_test() throws Exception {
 
         // given
-        Integer companysId = 1;
+        CompanysUpdateReqDto companysUpdateReqDto = new CompanysUpdateReqDto();
+        companysUpdateReqDto.setUsersId(1);
+        companysUpdateReqDto.setCompanyName("박하 농장");
+        companysUpdateReqDto.setAddress("서울 개포동");
+        companysUpdateReqDto.setEmail("aaa@nate.com");
+        companysUpdateReqDto.setRegionCodeName("부산");
+        companysUpdateReqDto.setMemberCount(22);
+
+        String body = om.writeValueAsString(companysUpdateReqDto);
+        System.out.println(body);
         // when
         ResultActions resultActions = mvc.perform(
-                MockMvcRequestBuilders.delete("/s/api/companys/" + companysId)
-                        .accept(APPLICATION_JSON)
-                        .session(session));
+                MockMvcRequestBuilders.put("/s/api/companys")
+                        .content(body)
+                        .contentType(APPLICATION_JSON)
+                        .session(session)
+                        .accept(APPLICATION_JSON));
+
         // then
+        MvcResult mvcResult = resultActions.andReturn();
+        System.out.println("debugggg:" + mvcResult.getResponse().getContentAsString());
+
         resultActions.andExpect(MockMvcResultMatchers.status().isOk());
         resultActions.andExpect(MockMvcResultMatchers.jsonPath("$.code").value(1));
-        resultActions.andExpect(MockMvcResultMatchers.jsonPath("$.data.companyName").value("남해어장횟집"));
-        resultActions.andExpect(MockMvcResultMatchers.jsonPath("$.data.address").value("경상남도 남해군 상주면 남해대로675번길 76"));
-        resultActions.andExpect(MockMvcResultMatchers.jsonPath("$.email").value("fish@nate.com"));
-        resultActions.andExpect(MockMvcResultMatchers.jsonPath("$.data.regionCodeName").value("남해"));
-        resultActions.andExpect(MockMvcResultMatchers.jsonPath("$.data.memberCount").value(11));
-        resultActions.andExpect(MockMvcResultMatchers.jsonPath("$.data.usersId").value(1));// 유저 아이디
+
+        // 유저 아이디
         // 회사정보 엡데이트 테스트
     }
 
@@ -168,37 +180,40 @@ public class TestCompanysApiController {
     @Test
     public void deleteCompanysId_test() throws Exception {
 
-        // given
-        Integer companysId = 1;
         // when
         ResultActions resultActions = mvc.perform(
-                MockMvcRequestBuilders.delete("/s/api/companys" + companysId + "/delete")
-                        .accept(APPLICATION_JSON)
-                        .session(session));
+                MockMvcRequestBuilders.delete("/s/api/companys/delete")
+                        .contentType(APPLICATION_JSON)
+                        .session(session)
+                        .accept(APPLICATION_JSON));
 
         // then
+        MvcResult mvcResult = resultActions.andReturn();
+        System.out.println("debugggg:" + mvcResult.getResponse().getContentAsString());
+
         resultActions.andExpect(MockMvcResultMatchers.status().isOk());
         resultActions.andExpect(MockMvcResultMatchers.jsonPath("$.code").value(1));
 
-    }// 회사서비스 삭제 테스트
+    }/// 회사서비스 삭제 테스트
 
     @Sql(scripts = "classpath:createTest.sql", executionPhase = ExecutionPhase.BEFORE_TEST_METHOD)
     @Test
     public void subscribesform_test() throws Exception {
 
-        // given
-        Integer userId = 1;
+        Integer UserId = 1;
+
         // when
         ResultActions resultActions = mvc.perform(
-                MockMvcRequestBuilders.delete("/s/mypage/" + userId + "/subscribes")
+                MockMvcRequestBuilders.get("/s/mypage/subscribes")
                         .accept(APPLICATION_JSON)
                         .session(session));
 
         // then
+        MvcResult mvcResult = resultActions.andReturn();
+        System.out.println("debugggg:" + mvcResult.getResponse().getContentAsString());
+
         resultActions.andExpect(MockMvcResultMatchers.status().isOk());
         resultActions.andExpect(MockMvcResultMatchers.jsonPath("$.code").value(1));
-        resultActions.andExpect(MockMvcResultMatchers.jsonPath("$.data.userId").value(1));
-
     }
 
 }
