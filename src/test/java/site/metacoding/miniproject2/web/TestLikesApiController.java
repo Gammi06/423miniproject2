780747c48c7d38197626b737e105c67d693fe9c0
mysjw1028 <108706248+mysjw1028@users.noreply.github.com>
2022-11-10 12:22:09 +0900

    Companys update수정 (#67)
    * Dto이름 수정
    
    * WantedSkillsRespDto 파일 위치 수정 완료
    
    * MySkills 이름 확인 했습니다.
    
    * Service기초세팅 완료
    
    * CompanysService 샘플코드완료(수정예정)
    
    * subcribes 수정할것
    
    * myskills xmㅣinsert추가
    
    * xml주소 수정/ 서비스 기초 틀 완료
    
    * dao부분 수정완료
    
    * 서비스/다오 엔티티부분 디티오 변경 완료
    
    * service 수정할것
    
    * service 추가수정
    
    * service 수정성공
    
    * xml 수정 완료
    
    * pull전 xml저장
    
    * Subcribes Companys로 위치변경
    
    * pull 후 수정
    
    * Subcribes 수정 완료
    
    * subribes수정 완료
    
    * 메소드명 수정완료
    
    * mapper 오류 확인 후 수정
    
    * updatecompany로 수정 완료
    
    * 컨트롤러 테스트 돌리기
    
    * 서버확인
    
    * 서버 돌아가는거 확인 완료
    
    * likeController중
    
    * Subcribes 작동확인 완료
    
    * 회사 업데이트 포스트맨 테스트 완료
    
    * 회사가입 확인완료
    
    * 회사정보 수정 하는 코드 수정
    
    * 사업자 중복체크 쿼리 스트링 체크 확인
    
    * 컨트롤러 테스트 완료
    
    * 컨트롤러 테스트 완료
    
    * 컨트롤러 확인
    
    * subcribesLis 쿼리 수정 완료
    
    * companys delete 수정 완료!
    
    * likes 작업중
    
    * likes완료 dto.dao.쿼리추가
    
    * subscribes 주소변경 완료
    
    * 컨트롤러 / 좋아요페이지 주소 수정 완료
    
    * subscribes /likes의 주소 mypage 위치 변경
    
    * photo 수정 완료 (company insert쿼리)
    
    * 회사정보 삭제시 구인공고 같이 삭제
    
    * test
    
    * principal 변경 완료
    
    * 인터셉터 수정예정
    
    * 회사정보수정 junit 테스트 완료
    
    * 구독하기 junit 수정중
    
    * principal 수정 완료
    
    * post맨 테스트 완료
    
    * subscribesform junit 테스트 완료
    
    * likes 수정 완료
    
    * likes junit 테스트 완료
    
    * 회사 삭제하기 완료
    
    * 확인 완료
    
    * Likes junit 테스트 코드 위치변경
    
    * 회사 정보 수정완료
    
    Co-authored-by: SINGIN <104334760+Gammi06@users.noreply.github.com>

---
+++ src/test/java/site/metacoding/miniproject2/web/TestLikesApiController.java
@@ -0,0 +1,74 @@
+package site.metacoding.miniproject2.web;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.mock.web.MockHttpSession;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import lombok.extern.slf4j.Slf4j;
+import site.metacoding.miniproject2.dto.SessionUsers;
+
+@Slf4j
+@ActiveProfiles("test") // 테스트 어플리케이션 실행
+@Sql("classpath:truncate.sql")
+@Transactional
+@AutoConfigureMockMvc // MockMvc Ioc 컨테이너에 등록 실제가 아닌 가짜
+@SpringBootTest(webEnvironment = WebEnvironment.MOCK) // MOCK은 가짜 환경임
+public class TestLikesApiController {
+
+    // header json
+    private static final String APPLICATION_JSON = "application/json; charset=utf-8";
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Autowired
+    private ObjectMapper om;
+
+    private MockHttpSession session;
+
+    @BeforeEach
+    public void sessionInit() {
+        session = new MockHttpSession();
+        session.setAttribute("principal", SessionUsers.builder()
+                .id(1)
+                .userId("garam1234")
+                .role("일반")
+                .build());
+    }
+
+    @Sql(scripts = "classpath:createTest.sql", executionPhase = ExecutionPhase.BEFORE_TEST_METHOD)
+    @Test
+    public void likesform_test() throws Exception {
+
+        Integer Id = 1;
+
+        // when
+        ResultActions resultActions = mvc.perform(
+                MockMvcRequestBuilders.get("/s/mypage/likes")
+                        .accept(APPLICATION_JSON)
+                        .session(session));
+
+        // then
+        MvcResult mvcResult = resultActions.andReturn();
+        System.out.println("debugggg:" + mvcResult.getResponse().getContentAsString());
+
+        resultActions.andExpect(MockMvcResultMatchers.status().isOk());
+        resultActions.andExpect(MockMvcResultMatchers.jsonPath("$.code").value(1));
+    }
+
+}
